59995fd9f2edb76b4672574b845427f8addada93
Amy Haywood Dutton <me@amyhaywood.com>
2024-03-06 09:09:56 -0600

    🎉 Initial Commit

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,41 +1,23 @@
 import type { MetaFunction } from "@remix-run/node";
+import videos from "~/lib/videos.json";
+import { Video } from "./Video";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
+    { title: "Cloudinary Video Preview" },
     { name: "description", content: "Welcome to Remix!" },
   ];
 };
 
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div className="grid grid-cols-5 gap-10">
+      <div className="col-span-5">
+        <img src="/images/logo.png" alt="YouTube Shorts" width="100" />
+      </div>
+      {videos.map((video, index) => {
+        return <Video video={video} key={index} />;
+      })}
     </div>
   );
 }
